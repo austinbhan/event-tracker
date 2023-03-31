7bc3ca2f59c5d5b52a5d22c3c4735c12a5acd59b
austinbhan <austinbhan@outlook.com>
2023-03-31 15:57:03 -0700

    Created but must link css

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,16 @@ import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 
+import './app.css';
+
+
 export default function App() {
   return(
-    <Router>
-      <h1>Hello World</h1>
-    </Router>
+    <div className="App">
+      <h1>This is the Home Page</h1>
+      <Router>
+        <h1>Hello World</h1>
+      </Router>
+    </div>
   );
 }
