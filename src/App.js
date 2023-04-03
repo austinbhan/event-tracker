a506da99b64f42b46b29bcc4c895c3a40f7d00c2
austinbhan <austinbhan@outlook.com>
2023-04-03 16:06:38 -0700

    Correctly linking css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
-
-import './app.css';
-
-
+import styles from './App.css';
+  
+  
 export default function App() {
   return(
     <div className="App">
       <h1>This is the Home Page</h1>
+      <div className={styles.trial}>
+        <p>
+            Hi 
+        </p>
+      </div>
       <Router>
         <h1>Hello World</h1>
       </Router>
